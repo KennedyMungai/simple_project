99beaa76aa9a6e75f37e654c94f3fae402b3c653
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-18 15:51:19 +0300

    Added some styles to the search bar

--- components/NavBar/Search.tsx
+++ components/NavBar/Search.tsx
@@ -1,15 +1,33 @@
 'use client'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-
+import { useRouter } from 'next/navigation'
+import { FormEvent, useState } from 'react'
 
 type Props = {}
 
 const Search = (props: Props) => {
 	const [search, setSearch] = useState('')
-    const router = useRouter()
+	const router = useRouter()
 
-	return <div>Search</div>
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		setSearch('')
+		router.push(`/search/${search}`)
+	}
+
+	return (
+		<form
+			className='w-50 flex justify-center md:justify-between'
+			onSubmit={handleSubmit}
+		>
+			<input
+				type='text'
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+                className='bg-white p-2 w-80 text-xl rounded-xl focus:outline-none text-center placeholder:text-sm placeholder:font-semibold'
+                placeholder='Enter Search Term'
+			/>
+		</form>
+	)
 }
 
 export default Search

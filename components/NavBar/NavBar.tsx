99bef0773d20c3acb43fb559864c16f16596f674
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-18 15:43:08 +0300

    Rendered the search component

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link"
+import Search from "./Search"
 
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <nav className='bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl'>
+	return <nav className='bg-slate-600 p-4 flex justify-between md:flex-row items-center sticky top-0 drop-shadow-xl'>
         <h1 className="uppercase text-2xl text-white font-semibold">
             <Link href={'/'}>WikiRocket</Link>
         </h1>
+        <Search />
     </nav>
 }
 

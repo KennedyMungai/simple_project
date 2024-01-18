5d9c5f62e29908c020269f2320acee14daf898c6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-18 15:31:28 +0300

    Added some markup and logic to the NavBar component

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from "next/link"
 
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <div className='h-[10vh] border-b'>NavBar</div>
+	return <nav className='h-[10vh] border-b flex justify-between items-center'>
+        <h1 className="uppercase text-2xl">
+            <Link href={'/'}>WikiRocket</Link>
+        </h1>
+    </nav>
 }
 
 export default NavBar

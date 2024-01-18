f29a155141f2c1203c4bfc38e9eb1f0efbdb3d51
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-18 15:59:58 +0300

    Made some styling changes

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import Search from "./Search"
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <nav className='bg-slate-600 p-4 flex justify-between md:flex-row items-center sticky top-0 drop-shadow-xl'>
+	return <nav className='bg-slate-600 p-4 flex flex-col justify-between md:flex-row items-center sticky top-0 drop-shadow-xl'>
         <h1 className="uppercase text-2xl text-white font-semibold">
             <Link href={'/'}>WikiRocket</Link>
         </h1>

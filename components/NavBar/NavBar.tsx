9f7c25762080e022d6aac86843e1f77d55121242
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-18 15:34:45 +0300

    Added some styles for the heading text

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const NavBar = (props: Props) => {
 	return <nav className='bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl'>
-        <h1 className="uppercase text-2xl">
+        <h1 className="uppercase text-2xl text-white font-semibold">
             <Link href={'/'}>WikiRocket</Link>
         </h1>
     </nav>

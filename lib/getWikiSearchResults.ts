13444473d27cb18b255c0f7c0a745f6eec3a9371
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-18 16:06:06 +0300

    Created the ts page to get the wiki search results

---
+++ lib/getWikiSearchResults.ts
@@ -0,0 +1,18 @@
+const getWikiSearchResults = (searchTerm: string) => {
+    const searchParams = new URLSearchParams({
+        action: 'query',
+        generator: 'search',
+        gsrsearch: searchTerm,
+        gsrlimit: '20',
+        prop: 'pageimages|extracts',
+        exchars: '200',
+        exintro: 'true',
+        explaintext: 'true',
+        exlimit: 'max',
+        format: 'json',
+        origin: '*'
+    })
+
+    const response = await fetch('https://en.wikipedia.org/w/api.php?'+searchParams)
+    return response.json()
+}

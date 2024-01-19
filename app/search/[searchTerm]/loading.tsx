d82759d7aba66e946ad765fcc7db7e6e3c7c4ebb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-19 10:30:59 +0300

    Created a loader for the searchTerm route

---
+++ app/search/[searchTerm]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const Loading = (props: Props) => {
+	return <div className='h-[90vh] flex items-center justify-center'>Loading...</div>
+}
+
+export default Loading

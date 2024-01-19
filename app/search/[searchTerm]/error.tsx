d81f7b1bf2bbdc006992342a7a68cd0e36a3e279
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-19 10:21:01 +0300

    Created the error component for the searchTerm dynamic route

---
+++ app/search/[searchTerm]/error.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+type Props = {}
+
+const SearchTermErrorPage = (props: Props) => {
+	return <div className="h-[90vh] flex items-center justify-center text-3xl font-semibold text-red-300">error</div>
+}
+
+export default SearchTermErrorPage
